d2d5d00df8fba46cc9ef932c6fa85c56c79779d2
RKG-01 <rhys.gaffar01@gmail.com>
2023-06-02 12:09:12 +0100

    Remaster.
    Remastered Home screen, started work on Photography page. And updated my age. Tis ma birthday.

--- js/index.js
+++ js/index.js
@@ -46,7 +46,6 @@ gsap.timeline({})
 .from("#scroll_down", {opacity:0, delay: 0, duration: 0.8, ease: 'Power1.easeInOut', onComplete: afterIntroAnim})
 
 
-
 //thirdSection
 gsap.timeline({ //Create a timeline.
    //Timeline properties
@@ -54,38 +53,22 @@ gsap.timeline({ //Create a timeline.
 }) 
 .from(".thirdSection", {opacity:0, delay: 0.5, duration: 0.8, ease: 'Power1.easeInOut', onComplete: bgAnim}) //Anime from properties. https://greensock.com/docs/v2/Easing
   
-//fourthSection
+//overviewSection
 gsap.timeline({
-  scrollTrigger: ".fourthSection",
+  scrollTrigger: ".overviewSection",
 })
-.from("#fourthSectionH", {paddingLeft:100, duration:0.8, delay: 0.1, ease: 'Power1.easeInOut'})
+.from("#overviewH", {paddingLeft:100, duration:0.8, delay: 0.1, ease: 'Power1.easeInOut'})
 
-//Images (Photography Section)
-gsap.timeline({
-  scrollTrigger: "#p1_p_img"
-})
-.to("#p1_p_h4", {letterSpacing:'4px', duration: 0.8, ease: 'Power1.easeInOut'})
-.from("#p1_p_img", {filter: 'blur(64px)', scale: 1, duration: 0.8, ease: 'Power1.easeInOut'})
+//Images in the OverviewSection.
+//Photography
+gsap.timeline({scrollTrigger: "#photo"})
+.to("#photoP", {letterSpacing:'4px', duration: 0.8, delay: 0.5, ease: CustomEase.create("easeInOutQuart", ".76,0,.24,1")})
+.from("#photoImg", {filter: 'blur(64px)', scale: 1, duration: 0.8,  ease: CustomEase.create("easeInOutQuad", "0,-0.04,0,1.05")})
+//Youtube
+gsap.timeline({scrollTrigger: "#yt"})
+.to("#ytP", {letterSpacing:'4px', duration: 0.8, delay: 0.5, ease: CustomEase.create("easeInOutQuart", ".76,0,.24,1")})
+.from("#ytImg", {filter: 'blur(64px)', scale: 1, duration: 0.8,  ease: CustomEase.create("easeInOutQuad", "0,-0.04,0,1.05")})
 
-gsap.timeline({
-  scrollTrigger: "#gtr_p_img"
-})
-.to("#gtr_p_h4", {letterSpacing:'4px', duration: 0.8, ease: 'Power1.easeInOut'})
-.from("#gtr_p_img", {filter: 'blur(64px)', scale: 1, duration: 0.8, ease: 'Power1.easeInOut'})
-
-gsap.timeline({
- scrollTrigger: "#irl_p_img"
- })
-.to("#irl_p_h4", {letterSpacing:'4px', duration: 0.8, ease: 'Power1.easeInOut'})
-.from("#irl_p_img", {filter: 'blur(64px)', scale: 1, duration: 0.8, ease: 'Power1.easeInOut'})
-
-
-//Fifth Section
-gsap.timeline({
-  scrollTrigger: ".fifthSection"
-})
-.to(".fifthSection", {backgroundColor: "#080808", duration: 0.8, ease: 'Power1.easeInOut'})
-.from("#fifthSectionH", {paddingLeft:100, duration:0.8, delay: 0.1, ease: 'Power1.easeInOut'})
 
 
 
